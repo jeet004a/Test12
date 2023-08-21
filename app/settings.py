5fdd2887bd1fd812c831012ec3ccf83f6920ea81
wastijeet110@gmail.com <ClinkRatCheat>
2023-08-22 02:35:13 +0530

    Final commit of the day

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ns6w4x*y*9a=mrjx^rrg$gbj2qvn!x2ml2scdl6*i%t)e=047
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
